32edd5ba56081a7d6cf07a2eee6c7cbe0b60690c
alfndp <alfndp25@gmail.com>
2022-05-22 13:04:56 +0900

    Initial commit

---
+++ src/main/java/com/catmunity/toy/CatmunityApplication.java
@@ -0,0 +1,13 @@
+package com.catmunity.toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatmunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatmunityApplication.class, args);
+	}
+
+}
