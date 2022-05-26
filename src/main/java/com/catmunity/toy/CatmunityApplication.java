6a0180bec96a50b8ef888fff4fddab25e5d97df2
alfndp <alfndp25@gmail.com>
2022-05-26 21:45:16 +0900

    초기 설정

--- src/main/java/com/catmunity/toy/CatmunityApplication.java
+++ src/main/java/com/catmunity/toy/CatmunityApplication.java
@@ -1,9 +1,15 @@
 package com.catmunity.toy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@Component
+//@ComponentScan(basePackageClasses = MemberController.class)
+//@MapperScan(basePackageClasses = MemberDAO.class)
 public class CatmunityApplication {
 
 	public static void main(String[] args) {
