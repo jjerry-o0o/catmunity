9c61af8c74d5393857c22de5acf1101400bdf2a9
alfndp <alfndp25@gmail.com>
2022-05-28 22:32:12 +0900

    main Controller 생성

--- src/main/java/com/catmunity/toy/CatmunityApplication.java
+++ src/main/java/com/catmunity/toy/CatmunityApplication.java
@@ -1,6 +1,5 @@
 package com.catmunity.toy;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,8 +7,7 @@ import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @Component
-//@ComponentScan(basePackageClasses = MemberController.class)
-//@MapperScan(basePackageClasses = MemberDAO.class)
+@ComponentScan(basePackageClasses = MainController.class)
 public class CatmunityApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/webapp/WEB-INF/views/main.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Hey,earth</title>
+</head>
+<body>
+<%@ include file="/WEB-INF/views/include/header.jsp"%>
+
+메인입니다
+
+<%@ include file="/WEB-INF/views/include/footer.jsp"%>
+</body>
+</html>
+
