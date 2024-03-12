bee88fef697c01774cf263e140781cc9367431a5
alfndp <alfndp25@gmail.com>
2024-03-12 22:36:20 +0900

    Initial commit

---
+++ src/main/java/com/toy/catmunity/CatmunityApplication.java
@@ -0,0 +1,13 @@
+package com.toy.catmunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatmunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatmunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toy/catmunity/CatmunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.catmunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatmunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
